627a0002b8e14b3249b5726b10a4679615fc9b19
Grzegorz Sztuczyński <19194188+farce1@users.noreply.github.com>
2022-02-04 00:15:53 +0100

    Create template overlap functions for weapon

--- Source/FirstGame/Weapon.cpp
+++ Source/FirstGame/Weapon.cpp
@@ -81,3 +81,13 @@ void AWeapon::Equip(AMain* Character)
 		}
 	}
 }
+
+void CombatOnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+
+}
+
+void CombatOnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+
+}
